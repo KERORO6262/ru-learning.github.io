9cdb248f7685f43603a4cc3bfbd2abc0530fd306
KERORO6262 <45860855+KERORO6262@users.noreply.github.com>
2026-02-13 10:50:32 +0800

    Add Russian alphabet letters data
    This file contains an IIFE that defines the Russian alphabet letters with their uppercase, lowercase, Russian names, and hints.

---
+++ js/data/letters.js
@@ -0,0 +1,39 @@
+(function () {
+  window.RuLettersApp = window.RuLettersApp || {};
+
+  window.RuLettersApp.LETTERS = [
+    { upper:"А", lower:"а", nameRu:"а", hint:"a" },
+    { upper:"Б", lower:"б", nameRu:"бэ", hint:"be" },
+    { upper:"В", lower:"в", nameRu:"вэ", hint:"ve" },
+    { upper:"Г", lower:"г", nameRu:"гэ", hint:"ge" },
+    { upper:"Д", lower:"д", nameRu:"дэ", hint:"de" },
+    { upper:"Е", lower:"е", nameRu:"е", hint:"ye / e" },
+    { upper:"Ё", lower:"ё", nameRu:"ё", hint:"yo" },
+    { upper:"Ж", lower:"ж", nameRu:"жэ", hint:"zhe" },
+    { upper:"З", lower:"з", nameRu:"зэ", hint:"ze" },
+    { upper:"И", lower:"и", nameRu:"и", hint:"i" },
+    { upper:"Й", lower:"й", nameRu:"и краткое", hint:"short i" },
+    { upper:"К", lower:"к", nameRu:"ка", hint:"ka" },
+    { upper:"Л", lower:"л", nameRu:"эл", hint:"el" },
+    { upper:"М", lower:"м", nameRu:"эм", hint:"em" },
+    { upper:"Н", lower:"н", nameRu:"эн", hint:"en" },
+    { upper:"О", lower:"о", nameRu:"о", hint:"o" },
+    { upper:"П", lower:"п", nameRu:"пэ", hint:"pe" },
+    { upper:"Р", lower:"р", nameRu:"эр", hint:"er" },
+    { upper:"С", lower:"с", nameRu:"эс", hint:"es" },
+    { upper:"Т", lower:"т", nameRu:"тэ", hint:"te" },
+    { upper:"У", lower:"у", nameRu:"у", hint:"u" },
+    { upper:"Ф", lower:"ф", nameRu:"эф", hint:"ef" },
+    { upper:"Х", lower:"х", nameRu:"ха", hint:"kha" },
+    { upper:"Ц", lower:"ц", nameRu:"цэ", hint:"tse" },
+    { upper:"Ч", lower:"ч", nameRu:"чэ", hint:"che" },
+    { upper:"Ш", lower:"ш", nameRu:"ша", hint:"sha" },
+    { upper:"Щ", lower:"щ", nameRu:"ща", hint:"shcha" },
+    { upper:"Ъ", lower:"ъ", nameRu:"твёрдый знак", hint:"hard sign" },
+    { upper:"Ы", lower:"ы", nameRu:"ы", hint:"yery" },
+    { upper:"Ь", lower:"ь", nameRu:"мягкий знак", hint:"soft sign" },
+    { upper:"Э", lower:"э", nameRu:"э", hint:"e" },
+    { upper:"Ю", lower:"ю", nameRu:"ю", hint:"yu" },
+    { upper:"Я", lower:"я", nameRu:"я", hint:"ya" }
+  ];
+})();
